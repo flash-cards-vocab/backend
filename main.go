38b76d40c3d3b87d672bab8d61aa3708ef7b7d16
Aitugan <mirash2001@gmail.com>
2022-08-17 14:30:41 +0600

    feat: initialized main file with routes inside. Created a function to connect to postgres DB

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	collection_uc "github.com/flash-cards-vocab/backend/app/usecase/collection"
+	user_uc "github.com/flash-cards-vocab/backend/app/usecase/user"
+	"github.com/flash-cards-vocab/backend/config"
+	"github.com/flash-cards-vocab/backend/handler"
+	"github.com/flash-cards-vocab/backend/repository/collection_repository"
+	"github.com/flash-cards-vocab/backend/repository/user_repository"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm/logger"
+
+	postgres "github.com/flash-cards-vocab/backend/pkg/database"
+)
+
+func main() {
+	cfg := config.New()
+	router := gin.Default()
+
+	db, err := postgres.Connect(cfg.DBHost, cfg.DBPort, cfg.DBUserName, cfg.DBPassword, cfg.DBDatabaseName, logger.LogLevel(cfg.DBLogMode))
+	if err != nil {
+		log.Panicln("Failed to Initialized postgres DB:", err)
+	}
+	// rdsPool, err := redis.Connect(cfg.RedisHost, cfg.RedisPort, "")
+	// if err != nil {
+	// 	log.Panicln("Failed to Initialized redis:", err)
+	// }
+	collection_repo := collection_repository.New(db)
+	collection_uc := collection_uc.New(collection_repo)
+
+	collectionHndlr := handler.NewCollectionHandler(collection_uc)
+
+	/* CRUD author for CMS */
+	collection := router.Group("/collection")
+	collection.GET("/my/:user_id", collectionHndlr.GetMyCollections)
+	collection.PUT("/like/:id", collectionHndlr.LikeCollectionById)
+	collection.PUT("/dislike/:id", collectionHndlr.DislikeCollectionById)
+	collection.PUT("/view/:id", collectionHndlr.ViewCollectionById)
+
+	user_repo := user_repository.New(db)
+	user_uc := user_uc.New(user_repo)
+
+	userHndlr := handler.NewUserHandler(user_uc)
+	user := router.Group("/user")
+	user.POST("/login", userHndlr.Login)
+	user.POST("/register", userHndlr.Register)
+	// user.PUT("/logout", userHndlr.DislikeCollectionById)
+	// user.PUT("/refresh-token", userHndlr.ViewCollectionById)
+
+	// card := router.Group("/card")
+	// card.GET("/get-first-ten", h.GetMyCollections)
+	// card.PUT("/register", h.LikeCollectionById)
+	// card.PUT("/logout", h.DislikeCollectionById)
+	// card.PUT("/refresh-token", h.ViewCollectionById)
+
+	router.Run(fmt.Sprintf(":%d", cfg.Port))
+}
