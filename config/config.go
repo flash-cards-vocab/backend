8ee9ea2979a3b5866a45fb0eb87d46c987b43434
Aitugan <mirash2001@gmail.com>
2022-08-17 14:11:54 +0600

    build: initialized configs for DB and API

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	ServiceName string `envconfig:"SERVICE_NAME" default:"rest-api"`
+	Environment string `envconfig:"ENV" default:"dev"`
+	Port        int    `envconfig:"PORT" default:"8080" required:"true"`
+
+	DBHost         string `envconfig:"DB_HOST" default:"localhost"`
+	DBPort         string `envconfig:"DB_PORT" default:"33062"`
+	DBUserName     string `envconfig:"DB_USERNAME" default:"root"`
+	DBPassword     string `envconfig:"DB_PASSWORD" default:"password"`
+	DBDatabaseName string `envconfig:"DB_DBNAME" default:"gca"`
+	DBLogMode      int    `envconfig:"DB_LOG_MODE" default:"3"`
+
+	RedisHost string `envconfig:"REDIS_HOST" default:"localhost"`
+	RedisPort string `envconfig:"REDIS_PORT" default:"33792"`
+}
+
+func New() Config {
+	cfg := Config{}
+	envconfig.MustProcess("", &cfg)
+	cfg.DBPassword = "password"
+	return cfg
+}
