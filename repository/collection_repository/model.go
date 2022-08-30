f573b3d857648f83d024f922ca37d0408e85ecab
Aitugan <mirash2001@gmail.com>
2022-08-30 18:37:32 +0600

    feat: updated collection progress and metrics models, created repo functions to retrieve total amount of cards in collection, recommended collection, repos to star collection by its id, get collection user progress and metrics and to create collection

--- repository/collection_repository/model.go
+++ repository/collection_repository/model.go
@@ -5,33 +5,36 @@ import (
 
 	"github.com/flash-cards-vocab/backend/entity"
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 type Collection struct {
-	Id        uuid.UUID  `gorm:"primary_key;column:id"`
-	Name      string     `gorm:"column:name"`
-	AuthorId  uuid.UUID  `gorm:"column:author_id"`
-	Topics    []string   `gorm:"column:topics"`
-	CreatedAt time.Time  `gorm:"column:created_at"`
-	UpdatedAt time.Time  `gorm:"column:updated_at"`
-	DeletedAt *time.Time `gorm:"column:deleted_at"`
+	Id        uuid.UUID      `gorm:"primary_key;column:id"`
+	Name      string         `gorm:"column:name"`
+	AuthorId  uuid.UUID      `gorm:"column:author_id"`
+	Topics    pq.StringArray `gorm:"type:text[];column:topics"`
+	CreatedAt time.Time      `gorm:"column:created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at"`
+	DeletedAt *time.Time     `gorm:"column:deleted_at"`
 }
 
 func (c *Collection) ToEntity() *entity.Collection {
 	return &entity.Collection{
-		Id:     c.Id,
-		Name:   c.Name,
-		Topics: c.Topics,
+		Id:        c.Id,
+		Name:      c.Name,
+		Topics:    c.Topics,
+		AuthorId:  c.AuthorId,
+		CreatedAt: c.CreatedAt,
 	}
 }
 
 type CollectionUserProgress struct {
-	Id           uuid.UUID `json:"id,omitempty"`
-	CollectionId uuid.UUID `json:"collection_id,omitempty"`
-	UserId       uuid.UUID `json:"user_id,omitempty"`
-	Mastered     uint32    `json:"mastered,omitempty"`
-	Reviewing    uint32    `json:"reviewing,omitempty"`
-	Learning     uint32    `json:"learning,omitempty"`
+	Id           uuid.UUID `gorm:"primary_key;column:id"`
+	CollectionId uuid.UUID `gorm:"column:collection_id"`
+	UserId       uuid.UUID `gorm:"column:user_id"`
+	Mastered     uint32    `gorm:"column:mastered"`
+	Reviewing    uint32    `gorm:"column:reviewing"`
+	Learning     uint32    `gorm:"column:learning"`
 }
 
 func (c *CollectionUserProgress) ToEntity() *entity.CollectionUserProgress {
@@ -46,29 +49,33 @@ func (c *CollectionUserProgress) ToEntity() *entity.CollectionUserProgress {
 }
 
 type CollectionUserMetrics struct {
-	Id           uuid.UUID `json:"id,omitempty"`
-	CollectionId uuid.UUID `json:"collection_id,omitempty"`
-	Liked        bool      `json:"liked,omitempty"`
-	Disliked     bool      `json:"disliked,omitempty"`
-	Viewed       bool      `json:"viewed,omitempty"`
+	Id           uuid.UUID `gorm:"primary_key;column:id"`
+	UserId       uuid.UUID `gorm:"column:user_id"`
+	CollectionId uuid.UUID `gorm:"column:collection_id"`
+	Liked        bool      `gorm:"column:liked"`
+	Disliked     bool      `gorm:"column:disliked"`
+	Viewed       bool      `gorm:"column:viewed"`
+	Starred      bool      `gorm:"column:starred"`
 }
 
 func (c *CollectionUserMetrics) ToEntity() *entity.CollectionUserMetrics {
 	return &entity.CollectionUserMetrics{
 		Id:           c.Id,
+		UserId:       c.UserId,
 		CollectionId: c.CollectionId,
 		Liked:        c.Liked,
 		Disliked:     c.Disliked,
 		Viewed:       c.Viewed,
+		Starred:      c.Starred,
 	}
 }
 
 type CollectionMetrics struct {
-	Id           uuid.UUID `json:"id,omitempty"`
-	CollectionId uuid.UUID `json:"collection_id,omitempty"`
-	Likes        uint32    `json:"likes,omitempty"`
-	Dislikes     uint32    `json:"dislikes,omitempty"`
-	Views        uint32    `json:"views,omitempty"`
+	Id           uuid.UUID `gorm:"primary_key;column:id"`
+	CollectionId uuid.UUID `gorm:"column:collection_id"`
+	Likes        uint32    `gorm:"column:likes"`
+	Dislikes     uint32    `gorm:"column:dislikes"`
+	Views        uint32    `gorm:"column:views"`
 }
 
 func (c *CollectionMetrics) ToEntity() *entity.CollectionMetrics {
@@ -82,9 +89,9 @@ func (c *CollectionMetrics) ToEntity() *entity.CollectionMetrics {
 }
 
 type CollectionCards struct {
-	Id           uuid.UUID `json:"id,omitempty"`
-	CardId       uuid.UUID `json:"card_id,omitempty"`
-	CollectionId uuid.UUID `json:"collection_id,omitempty"`
+	Id           uuid.UUID `gorm:"primary_key;column:id"`
+	CardId       uuid.UUID `gorm:"column:card_id"`
+	CollectionId uuid.UUID `gorm:"column:collection_id"`
 }
 
 func (c *CollectionCards) ToEntity() *entity.CollectionCards {
@@ -93,4 +100,45 @@ func (c *CollectionCards) ToEntity() *entity.CollectionCards {
 		CardId:       c.CardId,
 		CollectionId: c.CollectionId,
 	}
+}
+
+type Card struct {
+	Id         uuid.UUID  `gorm:"primary_key;column:id"`
+	Word       string     `gorm:"column:word"`
+	ImageUrl   string     `gorm:"column:image_url"`
+	Definition string     `gorm:"column:definition"`
+	Sentence   string     `gorm:"column:sentence"`
+	Antonyms   string     `gorm:"column:antonyms"`
+	Synonyms   string     `gorm:"column:synonyms"`
+	CreatedAt  time.Time  `gorm:"column:created_at"`
+	UpdatedAt  time.Time  `gorm:"column:updated_at"`
+	DeletedAt  *time.Time `gorm:"column:deleted_at"`
+}
+
+func (c *Card) ToEntity() *entity.Card {
+	return &entity.Card{
+		Id:         c.Id,
+		Word:       c.Word,
+		ImageUrl:   c.ImageUrl,
+		Definition: c.Definition,
+		Sentence:   c.Sentence,
+		Antonyms:   c.Antonyms,
+		Synonyms:   c.Synonyms,
+	}
+}
+
+func (c Card) ToArrayEntity(cards []*Card) []*entity.Card {
+	res := []*entity.Card{}
+	for _, card := range cards {
+		res = append(res, &entity.Card{
+			Id:         card.Id,
+			Word:       card.Word,
+			ImageUrl:   card.ImageUrl,
+			Definition: card.Definition,
+			Sentence:   card.Sentence,
+			Antonyms:   card.Antonyms,
+			Synonyms:   card.Synonyms,
+		})
+	}
+	return res
 }
--- repository/collection_repository/repository.go
+++ repository/collection_repository/repository.go
@@ -1,6 +1,7 @@
 package collection_repository
 
 import (
+	"errors"
 	"time"
 
 	repository_intf "github.com/flash-cards-vocab/backend/app/repository"
@@ -23,7 +24,7 @@ func (r *repository) GetMyCollections(user_id uuid.UUID) ([]*entity.Collection, 
 		Raw(`
 			SELECT * FROM collection
 			WHERE author_id = ?
-			AND deleted_at = null
+			AND deleted_at IS null
 		`, user_id).
 		Scan(&datas).
 		Error
@@ -37,11 +38,85 @@ func (r *repository) GetMyCollections(user_id uuid.UUID) ([]*entity.Collection, 
 	return resp, nil
 }
 
+func (r *repository) GetCollectionTotal(collection_id uuid.UUID) (int, error) {
+	var total *int
+	err := r.db.
+		Raw(`
+			SELECT COUNT(card.*) FROM card
+			INNER JOIN collection_cards ON collection_cards.card_id = card.id
+			INNER JOIN collection ON collection_cards.collection_id = collection.id
+			WHERE collection.id=?
+			AND card.deleted_at IS null
+		`, collection_id).
+		Scan(&total).
+		Error
+	if err != nil {
+		return 0, err
+	}
+
+	return *total, nil
+}
+
+func (r *repository) GetRecommendedCollectionsPreview(userId uuid.UUID) ([]*entity.Collection, error) {
+	datas := []Collection{}
+	err := r.db.
+		Raw(`
+			SELECT * FROM collection
+			WHERE author_id <> ? 
+			AND deleted_at IS null limit 10
+		`, userId).
+		Scan(&datas).
+		Error
+	if err != nil {
+		return nil, err
+	}
+	resp := []*entity.Collection{}
+	for _, data := range datas {
+		resp = append(resp, data.ToEntity())
+	}
+	return resp, nil
+}
+
+func (r *repository) StarCollectionById(id, userId uuid.UUID) error {
+	metrics := CollectionUserMetrics{}
+	err := r.db.
+		Table("collection_user_metrics").
+		Where("collection_id = ? AND user_id = ?", id, userId).
+		Scan(&metrics).
+		Error
+	if err != nil {
+		return err
+	}
+
+	if metrics.Starred {
+		err = r.db.
+			Table("collection_user_metrics").
+			Where("collection_id = ? AND user_id = ?", id, userId).
+			Updates(map[string]interface{}{
+				"starred": false,
+			}).Error
+		if err != nil {
+			return err
+		}
+	} else {
+		err = r.db.
+			Table("collection_user_metrics").
+			Where("collection_id = ? AND user_id = ?", id, userId).
+			Updates(map[string]interface{}{
+				"starred": true,
+			}).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (r *repository) IsCollectionLikedByUser(id, userId uuid.UUID) (bool, error) {
 	metrics := CollectionUserMetrics{}
 	err := r.db.
 		Table("collection_user_metrics").
-		Where("id = ? AND user_id = ?", id, userId).
+		Where("collection_id = ? AND user_id = ?", id, userId).
 		Scan(&metrics).
 		Error
 	if err != nil {
@@ -54,7 +129,7 @@ func (r *repository) IsCollectionDislikedByUser(id, userId uuid.UUID) (bool, err
 	metrics := CollectionUserMetrics{}
 	err := r.db.
 		Table("collection_user_metrics").
-		Where("id = ? AND user_id = ?", id, userId).
+		Where("collection_id = ? AND user_id = ?", id, userId).
 		Scan(&metrics).
 		Error
 	if err != nil {
@@ -67,7 +142,7 @@ func (r *repository) IsCollectionViewedByUser(id, userId uuid.UUID) (bool, error
 	metrics := CollectionUserMetrics{}
 	err := r.db.
 		Table("collection_user_metrics").
-		Where("id = ? AND user_id = ?", id, userId).
+		Where("collection_id = ? AND user_id = ?", id, userId).
 		Scan(&metrics).
 		Error
 	if err != nil {
@@ -80,7 +155,7 @@ func (r *repository) CollectionLikeInteraction(id, userId uuid.UUID, isLiked boo
 	metrics := CollectionMetrics{}
 	err := r.db.
 		Table("collection_metrics").
-		Where("id = ? AND user_id = ?", id, userId).
+		Where("collection_id = ?", id).
 		Scan(&metrics).
 		Error
 	if err != nil {
@@ -113,7 +188,8 @@ func (r *repository) CollectionLikeInteraction(id, userId uuid.UUID, isLiked boo
 			Table("collection_user_metrics").
 			Where("collection_id = ? AND user_id = ?", id, userId).
 			Updates(map[string]interface{}{
-				"liked": true,
+				"liked":    true,
+				"disliked": false,
 			}).
 			Error
 		if err != nil {
@@ -128,7 +204,7 @@ func (r *repository) CollectionDislikeInteraction(id, userId uuid.UUID, isDislik
 	metrics := CollectionMetrics{}
 	err := r.db.
 		Table("collection_metrics").
-		Where("id = ? AND user_id = ?", id, userId).
+		Where("collection_id = ?", id).
 		Scan(&metrics).
 		Error
 	if err != nil {
@@ -159,6 +235,7 @@ func (r *repository) CollectionDislikeInteraction(id, userId uuid.UUID, isDislik
 			Where("collection_id = ? AND user_id = ?", id, userId).
 			Updates(map[string]interface{}{
 				"disliked": true,
+				"liked":    false,
 			}).
 			Error
 		if err != nil {
@@ -173,7 +250,7 @@ func (r *repository) ViewCollection(id, userId uuid.UUID) error {
 	metrics := CollectionMetrics{}
 	err := r.db.
 		Table("collection_metrics").
-		Where("id = ? AND user_id = ?", id, userId).
+		Where("collection_id = ?", id).
 		Scan(&metrics).
 		Error
 	if err != nil {
@@ -193,17 +270,175 @@ func (r *repository) SearchCollectionByName(name string) error {
 	panic("Not implemented")
 }
 func (r *repository) CreateCollection(collection entity.Collection) (*entity.Collection, error) {
+	tx := r.db.Begin()
 	collection_model := Collection{
+		Id:        uuid.New(),
 		Name:      collection.Name,
 		Topics:    collection.Topics,
+		AuthorId:  collection.AuthorId,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
 
 	err := r.db.Table("collection").Create(collection_model).Error
 	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	user_progress := CollectionUserProgress{
+		Id:           uuid.New(),
+		CollectionId: collection_model.Id,
+		UserId:       collection_model.AuthorId,
+		Mastered:     0,
+		Reviewing:    0,
+		Learning:     0,
+	}
+	err = r.db.Table("collection_user_progress").Create(user_progress).Error
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	user_metrics := CollectionUserMetrics{
+		Id:           uuid.New(),
+		UserId:       collection_model.AuthorId,
+		CollectionId: collection_model.Id,
+		Liked:        false,
+		Disliked:     false,
+		Viewed:       true,
+	}
+	err = r.db.Table("collection_user_metrics").Create(user_metrics).Error
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	collection_metrics := CollectionMetrics{
+		Id:           uuid.New(),
+		CollectionId: collection_model.Id,
+		Likes:        0,
+		Dislikes:     0,
+		Views:        1,
+	}
+	err = r.db.Table("collection_metrics").Create(collection_metrics).Error
+	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
 	return collection_model.ToEntity(), nil
+}
+
+func (r *repository) GetCollectionMetrics(id uuid.UUID) (*entity.CollectionMetrics, error) {
+	metrics := CollectionMetrics{}
+	err := r.db.
+		Table("collection_metrics").
+		Where("collection_id = ?", id).
+		Scan(&metrics).
+		Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return &entity.CollectionMetrics{
+				CollectionId: id,
+				Likes:        0,
+				Dislikes:     0,
+				Views:        0,
+			}, nil
+		}
+		return nil, err
+	}
+	return metrics.ToEntity(), nil
+}
+
+func (r *repository) GetCollectionUserProgress(id, userId uuid.UUID) (*entity.CollectionUserProgress, error) {
+	metrics := CollectionUserProgress{}
+	err := r.db.
+		Table("collection_user_progress").
+		Where("collection_id = ? AND user_id = ?", id, userId).
+		Scan(&metrics).
+		Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return &entity.CollectionUserProgress{
+				CollectionId: id,
+				UserId:       userId,
+				Mastered:     0,
+				Reviewing:    0,
+				Learning:     0,
+			}, nil
+		}
+		return nil, err
+	}
+	return metrics.ToEntity(), nil
+}
+
+func (r *repository) GetCollectionUserMetrics(id, userId uuid.UUID) (*entity.CollectionUserMetrics, error) {
+	metrics := CollectionUserMetrics{}
+	err := r.db.
+		Table("collection_user_metrics").
+		Where("collection_id = ? AND user_id = ?", id, userId).
+		Scan(&metrics).
+		Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return &entity.CollectionUserMetrics{
+				CollectionId: id,
+				Liked:        false,
+				Disliked:     false,
+				Viewed:       false,
+			}, nil
+		}
+		return nil, err
+	}
+	return metrics.ToEntity(), nil
+
+}
+
+func (r *repository) GetCollection(id uuid.UUID) (*entity.Collection, error) {
+	data := &Collection{}
+	err := r.db.
+		Raw(`
+			SELECT * FROM collection
+			WHERE id = ?
+			AND deleted_at IS null
+		`, id).
+		Scan(&data).
+		Error
+	if err != nil {
+		return nil, err
+	}
+	return data.ToEntity(), nil
+}
+
+func (r *repository) GetCollectionCards(id uuid.UUID, limit, offset int) (*entity.CardsPagination, error) {
+	cards := []*Card{}
+	err := r.db.
+		Raw(`
+			SELECT card.* FROM card
+			INNER JOIN collection_cards ON collection_cards.card_id = card.id
+			INNER JOIN collection ON collection_cards.collection_id = collection.id
+			WHERE collection.id=?
+			AND card.deleted_at IS null
+			LIMIT ?
+			OFFSET ?
+		`, id, limit, offset).
+		Find(&cards).
+		Error
+	if err != nil {
+		return nil, err
+	}
+	var total int
+	err = r.db.Raw(`
+		SELECT COUNT(*) FROM card
+		INNER JOIN collection_cards ON collection_cards.card_id = card.id
+		INNER JOIN collection ON collection_cards.collection_id = collection.id
+		WHERE collection.id=?
+		AND card.deleted_at IS null
+	`, id).Scan(&total).Error
+	data := &entity.CardsPagination{
+		Cards: Card{}.ToArrayEntity(cards),
+		Page:  limit,
+		Size:  offset,
+		Total: total,
+	}
+	return data, nil
+
 }
