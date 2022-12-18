d3fca5a67d40343e2e3c1a56604437fb5277b138
Aitugan <mirash2001@gmail.com>
2022-12-18 23:02:51 +0600

    feat: added card user status field

--- entity/card.go
+++ entity/card.go
@@ -14,9 +14,20 @@ type Card struct {
 	Synonyms   string    `json:"synonyms,omitempty"`
 }
 
-type CardsPagination struct {
-	Cards []*Card `json:"cards,omitempty"`
-	Page  int     `json:"page,omitempty"`
-	Size  int     `json:"size,omitempty"`
-	Total int     `json:"total,omitempty"`
+type CardForUserPagination struct {
+	CardForUser []*CardForUser `json:"cards,omitempty"`
+	Page        int            `json:"page,omitempty"`
+	Size        int            `json:"size,omitempty"`
+	Total       int            `json:"total,omitempty"`
+}
+
+type CardForUser struct {
+	Id         uuid.UUID `json:"id,omitempty"`
+	Word       string    `json:"word,omitempty"`
+	ImageUrl   string    `json:"image_url,omitempty"`
+	Definition string    `json:"definition,omitempty"`
+	Sentence   string    `json:"sentence,omitempty"`
+	Antonyms   string    `json:"antonyms,omitempty"`
+	Synonyms   string    `json:"synonyms,omitempty"`
+	Status     string    `json:"status,omitempty"`
 }
